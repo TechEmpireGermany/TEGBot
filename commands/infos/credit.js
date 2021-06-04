2808b17370bbdb217a05619793c46a8437f2e011
Ben Westover <kwestover.kw@gmail.com>
2021-06-04 16:28:31 -0400

    Update Credits

--- commands/infos/credit.js
+++ commands/infos/credit.js
@@ -13,15 +13,16 @@ module.exports = {
                 color: 'PURPLE',
                 author: { name: "credits" },
                 fields: [
-                    {name: 'Moderation:', value: "Developed by: Patriot Zest "},
+                    {name: 'Moderation:', value: "Developed by: Patriot Zest"},
                     {name: 'Welcome:', value: "Developed by: Patriot Zest"},
                     {name: 'Suggestions:', value: "Developed by: Patriot Zest"},
                     {name: 'Custom-Commands:', value: "Developed by: Patriot Zest"},
                     {name: 'Swear-Filter:', value: "Developed by: Patriot Zest"},
                     {name: 'Music:', value: "Developed by: Philipp"},
                     {name: 'embed-messsages', value: "Developed by: Philipp"},
-                    {name: 'Bothosting:', value: "By Philipp"},
+                    {name: 'Bot hosting:', value: "Hosted by Philipp's Pentium"},
                     {name: 'Support Team Information:', value: "Developed by: BenTheTechGuy"},
+                    {name: 'Support Team Custom Commands:', value: "Contributed by: Gracey"},
                     {name: 'Project Organizer:', value: "BenTheTechGuy"}
                 ]
             },
