4cca7217df1fc67e1710cf3e728dcb51dc3877e7
Philipp-spec <78240983+Philipp-spec@users.noreply.github.com>
2021-06-22 09:40:45 +0200

    Implementing a Activity Points system

---
+++ commands/infos/activity-list.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'activity-list',
+    aliases: [`activity`],
+    category: 'Infos',
+    utilisation: '{prefix}activity-list',
+
+
+ async   execute (client,message) {
+
+    const Levels = require("discord.js-leveling")
+
+
+
+
+    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); 
+
+    if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
+    
+    const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); 
+    
+    const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nActivity: ${e.level}\nActivity Points: ${e.xp.toLocaleString()}`); 
+    
+
+
+    message.channel.send(`**Activity**:\n\n${lb.join("\n\n")}`);
+
+
+
+
+
+ }}
