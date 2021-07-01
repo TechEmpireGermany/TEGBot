9f4defd9ae22b1bd0a7af5fcd09c6fef321cf5f9
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2021-07-01 08:48:19 +0530

    reaction role

---
+++ commands/react to me/reactionrole.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+module.exports = {
+    name: "reactrole",
+    description: "react a role",
+    async execute(client, message, args){
+        const reactionMessage = await message.channel.send("React for bump pings role");
+    try {
+      await reactionMessage.react("✅");
+    } catch (err) {
+      message.channel.send("err");
+      throw err;
+    } 
+        let role = message.member.guild.roles.cache.find(role => role.name === "Bump Reminders");
+        const collector = reactionMessage.createReactionCollector(
+            (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id),
+            { dispose: true }
+          );
+        
+          collector.on("collect", (reaction, user) => {
+            if(reaction.emoji.name == "✅"){
+            if(!message.guild.members.cache.get(user.id).roles.cache.has(role.id)){
+            message.guild.members.cache.get(user.id).roles.add(role)
+            user.send(`Role added ${role.name}`)
+            }
+          if(message.guild.members.cache.get(user.id).roles.cache.has(role.id)){
+              message.guild.members.cache.get(user.id).roles.remove(role)
+              user.send(`Role removed ${role.name}`)
+          }
+        }
+    })
+}
+}
