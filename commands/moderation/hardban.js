16f98933de35c2b6a5e4cb42551002e2252e67d0
Philipp-spec <78240983+Philipp-spec@users.noreply.github.com>
2021-07-16 08:47:04 +0200

    Fixing zest's returns

--- commands/moderation/hardban.js
+++ commands/moderation/hardban.js
@@ -4,8 +4,8 @@ module.exports = {
 	name: "hardban",
 	description: "Bans a member from a server",
 	async execute (client, message, args){
-	if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You don't have the right perms");
-	if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("I dont have the perms to ban");
+	if(!message.member.hasPermission("BAN_MEMBERS")) return 
+	if(!message.guild.me.hasPermission("BAN_MEMBERS")) return
 
 	let reason = args.slice(1).join(" ")
 	const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
@@ -13,7 +13,7 @@ module.exports = {
 	if(!reason) reason = "Unspecified";
 	if(!args[0]) return message.channel.send("You must specify a person to ban")
 	if(!mentioned) return message.channel.send("That user does not exist try again")
-	if(!mentioned.bannable) return message.channel.send("That user has a role which is above my role or is a mod/admin");
+	if(!mentioned.bannable) return
 
 	const embed = new Discord.MessageEmbed()
 	.setTitle('Banned')
