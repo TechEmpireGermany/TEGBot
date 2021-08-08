a7b10d67dbceb8079364f27ac66dec6410b596f7
Philipp-spec <subwayphil801@gmail.com>
2021-08-08 21:33:37 +0200

    Set Bot presence

--- config/bot.js
+++ config/bot.js
@@ -10,7 +10,7 @@ module.exports = {
     discord: {
         token: 'GET',
         prefix: '!',
-        activity: 'TSC',
+        watching: 'TSC',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
