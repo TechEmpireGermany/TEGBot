7b2631008d813eececb97a9082964f0ff5ab0311
Philipp-Spec <subwayphil801@gmail.com>
2021-08-22 14:29:30 +0200

    Update little Assistant

--- commands/core/help.js
+++ commands/core/help.js
@@ -8,10 +8,6 @@ module.exports = {
         if (!args[0]) {
             const infos = message.client.commands.filter(x => x.category == 'Infos').map((x) => '`' + x.name + '`').join(', ');
             const music = message.client.commands.filter(x => x.category == 'Music').map((x) => '`' + x.name + '`').join(', ');
-            const staff_re = message.client.commands.filter(x => x.category == 'staff-re').map((x) => '`' + x.name + '`').join(', ');
-            const messages = message.client.commands.filter(x => x.category == 'messages').map((x) => '`' + x.name + '`').join(', ');
-            const TODO = message.client.commands.filter(x => x.category == 'td').map((x) => '`' + x.name + '`').join(', ');
-            const spteam_commands = message.client.commands.filter(x => x.category == 'spteam-commands').map((x) => '`' + x.name + '`').join(', ');
 
             message.channel.send({
                 embed: {
@@ -21,10 +17,9 @@ module.exports = {
                     fields: [
                         { name: 'Infos', value: infos },
                         { name: 'Music', value: music },
-                        {name: 'Staff-re', value:staff_re },
-                        {name: 'Support Team Commands', value:spteam_commands },
-                        {name: 'messages', value:messages },
-                        {name: 'To-Do', value:TODO },
+                      
+                       
+                       
                         { name: 'Music Filters', value: client.filters.map((x) => '`' + x + '`').join(', ') },
                     ],
                     timestamp: new Date(),
--- commands/infos/credit.js
+++ commands/infos/credit.js
@@ -13,31 +13,13 @@ module.exports = {
                 color: 'PURPLE',
                 author: { name: "credits" },
                 fields: [
-                    {name: 'Moderation:', value: "Developed by: Patriot Zest"},
-                    {name: 'Welcome:', value: "Developed by: Patriot Zest"},
-                    {name: 'Suggestions:', value: "Developed by: Patriot Zest"},
-                    {name: 'Custom-Commands:', value: "Developed by: Patriot Zest"},
-                    {name: 'Swear-Filter:', value: "Developed by: Patriot Zest"},
-                    {name: 'Role commands:', value: "Developed by Patriot Zest" },
-                    {name: 'Bump Reminder:', value: "Developed by Patriot Zest" },
-                    {name: 'Reaction Role:', value: "Developed by Patriot Zest" },
-                    {name: 'Database Organization:', value: "Organization by: Patriot Zest" },
-                    {name: 'Swear List:', value: "Topaz, converted and improved by BenTheTechGuy"},
-                    {name: 'swear-filter improvements:', value: "Improved by: Philipp"},
-                    {name: 'Activity Points System:', value: "Developed by Philipp" },
-                    {name: 'Logs:', value: "Developed by Philipp" },
-                    {name: 'To-Do System:', value: "Developed by Philipp" },
-                    {name: 'Music:', value: "Developed by: Philipp"},
-                    {name: 'embed-messsages', value: "Developed by: Philipp"},
-                    {name: 'Auto-Roles:', value: "Developed by Philipp" },
-                    {name: 'Auto-response', value: "Developed by: Philipp"},
-                    {name: 'Bot hosting:', value: "Hosted by Philipp's Pentium"},
-                    {name: 'Support Team Information:', value: "Developed by: BenTheTechGuy"},
-                    {name: 'Support Team Custom Commands:', value: "Contributed by: Gracey"},
-                    {name: 'Website:', value: "Created and Designed by: BenTheTechGuy"},
-                    {name: 'Project Organizer:', value: "BenTheTechGuy"},
-                ]
-            },
+                   {name: "Py-mainbot" , value: "Developed by BenTheTechGuy"},
+                   {name: "Py assistant (Music ,Staff activity Tracking, credits,Tickets)  " , value : "by Philipp" },
+                   {name: "Bot hosting" , value : "by Philipp"}
+            
+                ],
+
+                },
     
       
         })
